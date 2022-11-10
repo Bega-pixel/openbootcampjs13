b988d82d237881e26d637cdca75f88401d23d37d
Bega-pixel <bega2000@protonmail.com>
2022-11-10 13:33:34 +0100

    ejer13

--- ejer13/index.js
+++ ejer13/index.js
@@ -8,3 +8,22 @@ const primNom = nombre[0];
 const ultiNom = apellido[-1];
 let espacioEstudiante = estudiante.join();
 const estudianteNombre = estudiante.includes(nombre);
+/*
+//sin errores
+const nombre = "Eduard"
+const apellido = "Eduawrds"
+const estudiante = `${nombre} ${apellido}`
+const estudianteMayus = estudiante.toLocaleUpperCase()
+const estudianteMinus = estudiante.toLocaleLowerCase()
+console.log(estudianteMayus+estudianteMinus)
+const numLetras = estudiante.length()
+const primNom = nombre[0]
+const ultiNom = apellido[-1]
+console.log(numLetras)
+console.log(primNom)
+console.log(ultiNom)
+let espacioEstudiante = estudiante.join()
+const estudianteNombre = estudiante.includes(nombre)
+console.log(espacioEstudiante)
+console.log(estudianteNombre)
+*/
